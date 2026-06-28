343fa110f812af85e0d2c9d7953b857ad1fd99aa
kobietdatten100-sketch <kobietdatten100@gmail.com>
2026-06-28 16:02:20 +0700

    Update colors.js

--- utils/colors.js
+++ utils/colors.js
@@ -1,1 +1,17 @@
+module.exports={
 
+    casino:0x8e44ad,
+
+    success:0x2ecc71,
+
+    error:0xe74c3c,
+
+    info:0x3498db,
+
+    warning:0xf1c40f,
+
+    wolf:0x6c3483,
+
+    jackpot:0xf39c12
+
+};

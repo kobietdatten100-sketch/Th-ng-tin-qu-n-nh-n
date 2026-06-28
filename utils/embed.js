8e159c6daf6a77fe565fc88448bcfff027052c1c
kobietdatten100-sketch <kobietdatten100@gmail.com>
2026-06-28 16:01:30 +0700

    Update embed.js

--- utils/embed.js
+++ utils/embed.js
@@ -1,1 +1,43 @@
+const {
 
+    EmbedBuilder
+
+} = require("discord.js");
+
+const config = require("../config");
+
+function createCasinoEmbed({
+
+    title,
+
+    description,
+
+    color,
+
+    footer
+
+}){
+
+    return new EmbedBuilder()
+
+        .setColor(color || config.colors.casino)
+
+        .setTitle(title)
+
+        .setDescription(description)
+
+        .setFooter({
+
+            text: footer || "PSCOIN CASINO"
+
+        })
+
+        .setTimestamp();
+
+}
+
+module.exports={
+
+    createCasinoEmbed
+
+};
